5c8bf341b3d2446b66a0c19ee0c04d0ab34f85af
shivakumar <42688381+shiva9596@users.noreply.github.com>
2025-04-15 01:24:35 -0700

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import tempfile
 
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.embeddings import CohereEmbeddings
-from langchain.vectorstores import FAISS
+from langchain_community.vectorstores import FAISS
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import RetrievalQA
 from langchain.prompts import PromptTemplate
